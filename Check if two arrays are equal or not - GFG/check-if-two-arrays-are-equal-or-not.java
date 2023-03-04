2c95a9d6f794ca5c4773e1ecb21d81d9325dc62d
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-03-04 20:28:32 +0530

    Added solution - LeetHub

---
+++ Check if two arrays are equal or not - GFG/check-if-two-arrays-are-equal-or-not.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0)
+		{
+		  int n=sc.nextInt();
+		  long arr[]=new long[n];
+		  long brr[]=new long[n];
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      arr[i]=sc.nextLong();
+		  }
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      brr[i]=sc.nextLong();
+		  }
+		  Solution ob = new Solution();
+		  System.out.println(ob.check(arr,brr,n)==true?"1":"0");
+		  
+		  
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    //Function to check if two arrays are equal or not.
+    public static boolean check(long A[],long B[],int N)
+    {
+        //Your code here
+          // if the size of the arrays is different, they are not equal
+        if (A.length != B.length) {
+            return false;
+        }
+        // create HashMap to store the frequency of each element in array a
+        HashMap<Long, Integer> freqMap = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            if (freqMap.containsKey(A[i])) {
+                freqMap.put(A[i], freqMap.get(A[i]) + 1);
+            } else {
+                freqMap.put(A[i], 1);
+            }
+        }
+        // check if each element in array b has the same frequency in array a
+        for (int i = 0; i < N; i++) {
+            if (freqMap.containsKey(B[i]) && freqMap.get(B[i]) > 0) {
+                freqMap.put(B[i], freqMap.get(B[i]) - 1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
