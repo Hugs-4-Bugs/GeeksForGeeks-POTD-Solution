b91bef8640600a5a942ce9d3fc0c159194bfa980
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-04-17 00:13:35 +0530

    Added solution - LeetHub

---
+++ Job Sequencing Problem - GFG/job-sequencing-problem.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
+            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// class Solution
+// {
+//     //Function to find the maximum profit and the number of jobs done.
+//     int[] JobScheduling(Job arr[], int n)
+//     {
+//         // Your code here
+
+class JobComparator implements Comparator<Job> {
+    public int compare(Job j1, Job j2) {
+        return j2.profit - j1.profit;
+    }
+}
+
+public class Solution {
+    int[] JobScheduling(Job[] jobs, int n) {
+        Arrays.sort(jobs, new JobComparator());
+        
+        int[] slots = new int[n];
+        Arrays.fill(slots, -1);
+        
+        int count = 0, profit = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = Math.min(n, jobs[i].deadline) - 1; j >= 0; j--) {
+                if (slots[j] == -1) {
+                    slots[j] = i;
+                    count++;
+                    profit += jobs[i].profit;
+                    break;
+                }
+            }
+        }
+        
+        int[] ans = new int[2];
+        ans[0] = count;
+        ans[1] = profit;
+        return ans;
+    }
+}
