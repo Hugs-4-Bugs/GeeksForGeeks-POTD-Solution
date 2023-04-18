d4d5fe8e9e79686be9a2779dab646b8eef835b7b
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-04-19 00:48:34 +0530

    Added solution - LeetHub

---
+++ Wifi Range - GFG/wifi-range.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(input_line[0]);
+            int X = Integer.parseInt(input_line[1]);
+            String S = read.readLine().trim();
+            Solution ob = new Solution();
+            boolean ans = ob.wifiRange(N, S, X); 
+            if(ans)
+            System.out.println(1);
+            else
+            System.out.println(0);
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    boolean wifiRange(int N, String S, int X) 
+    { 
+        // code here
+        boolean[] wifiRange = new boolean[N];
+        
+        for(int i=0; i<N; i++) {
+            if(S.charAt(i) == '1') {
+                int left = Math.max(0, i-X);
+                int right = Math.min(N-1, i+X);
+                for(int j=left; j<=right; j++) {
+                    wifiRange[j] = true;
+                }
+            }
+        }
+        
+        for(int i=0; i<N; i++) {
+            if(!wifiRange[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+} 
