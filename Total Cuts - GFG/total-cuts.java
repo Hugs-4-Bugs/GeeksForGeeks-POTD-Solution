5abf59575129e02dd90597084b5a12b77e5c9742
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-05-10 00:11:54 +0530

    Added solution - LeetHub

---
+++ Total Cuts - GFG/total-cuts.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int K;
+            K = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.totalCuts(N, K, A);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int totalCuts(int N, int K, int[] A) {
+        // code here
+        int count = 0;
+        int maxLeft = A[0];
+        int[] minRight = new int[N];
+        minRight[N-1] = A[N-1];
+        for (int i = N-2; i >= 0; i--) {
+            minRight[i] = Math.min(minRight[i+1], A[i]);
+        }
+        for (int i = 1; i < N; i++) {
+            maxLeft = Math.max(maxLeft, A[i-1]);
+            if (maxLeft + minRight[i] >= K) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+        
