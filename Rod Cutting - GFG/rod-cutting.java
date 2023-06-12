1cac1d7effd0b846ff80c10466d13612dd121907
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-06-12 14:46:26 +0530

    Added solution - LeetHub

---
+++ Rod Cutting - GFG/rod-cutting.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class RodCutting {
+
+    public static void main(String args[])throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
+
+            Solution ob = new Solution();
+            out.println(ob.cutRod(arr, n));
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    public int cutRod(int price[], int n) {
+        //code here
+        int val[] = new int[n + 1];
+        val[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int max_val = Integer.MIN_VALUE;
+            for (int j = 0; j < i; j++)
+                max_val = Math.max(max_val, price[j] + val[i - j - 1]);
+            val[i] = max_val;
+        }
+        return val[n];
+    }
+}
