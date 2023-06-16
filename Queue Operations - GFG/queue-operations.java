7c1980c88a57c0cda4d5fecd32a89ced2d44d037
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-06-17 01:46:31 +0530

    Added solution - LeetHub

---
+++ Queue Operations - GFG/queue-operations.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+// Helper class Geeks to implement 
+// insert() and findFrequency()
+class Geeks{
+    
+    // Function to insert element into the queue
+    static void insert(Queue<Integer> q, int k){
+        
+        // Your code here
+        q.add(k);
+        
+    }
+    
+    // Function to find frequency of an element
+    // rteturn the frequency of k
+    static int findFrequency(Queue<Integer> q, int k){
+        
+        // Your code here
+         return Collections.frequency(q, k);
+    }
+    
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        
+        insert(queue, 1);
+        insert(queue, 2);
+        insert(queue, 3);
+        insert(queue, 2);
+        insert(queue, 4);
+        insert(queue, 2);
+        
+        int number = 2;
+        int frequency = findFrequency(queue, number);
+        System.out.println("Frequency of " + number + ": " + frequency);
+        
+    }
+    
+}
+
+//{ Driver Code Starts.
+
+// Driver class with driver code
+class GFG {
+    
+    // Driver code
+	public static void main (String[] args) {
+		
+		// Taking input using scanner class
+		Scanner sc = new Scanner(System.in);
+		int testcase = sc.nextInt();
+		
+		while(testcase-- > 0){
+		    // Declaring Queue
+		    Queue<Integer> q = new LinkedList<>();
+		    int n = sc.nextInt();
+		    
+		    // Invoking object of Geeks class
+		    Geeks obj = new Geeks();
+		    
+		    for(int i = 0;i<n;i++){
+		        int k = sc.nextInt();
+		        obj.insert(q, k);
+		    }
+		   
+		    int x = sc.nextInt();
+		    for(int i = 0;i<x;i++){
+		        int k = sc.nextInt();
+		        int f = obj.findFrequency(q, k);
+		        if(f != 0){
+		            System.out.println(f);
+		        }
+		        else{
+		            System.out.println("-1");
+		        }
+		    }
+		}
+	}
+}
+// } Driver Code Ends
