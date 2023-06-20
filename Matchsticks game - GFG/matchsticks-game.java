d63b9b9eafed0d2dc6e06406a8e54e396cf02356
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-06-20 14:07:05 +0530

    Added solution - LeetHub

---
+++ Matchsticks game - GFG/matchsticks-game.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long N = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.matchGame(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int matchGame(Long N) {
+        // code here
+         // If not divisible by 5, then return the remainder
+        // when divided by 5 else return -1
+        if(N%5 !=0){
+            return(int)(N%5);
+        } else {
+            return -1;
+        }
+     
+    }
+};
+
+ 
+        
