ef5f8a4018c4ab5730aab3514a79f04db50869cd
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-07-03 08:19:46 +0530

    Added solution - LeetHub

---
+++ Maximum Index - GFG/maximum-index.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            System.out.println(new Solution().maxIndexDiff(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    int maxIndexDiff(int arr[], int n) {
+        // code here
+         int i=0,j=n-1,diff=0;
+        while(i<j){
+            if(arr[i]<=arr[j]){
+                diff=Math.max(diff,j-i);
+                i++;
+                j=n-1;
+            }else{
+                j--;
+            }
+        }
+        return diff;
+    }
+}
