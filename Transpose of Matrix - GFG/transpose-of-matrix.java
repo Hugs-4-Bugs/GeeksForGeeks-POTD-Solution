03d0823666386cae3bdf919915de013eaf11c73f
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-07-10 15:56:11 +0530

    Added solution - LeetHub

---
+++ Transpose of Matrix - GFG/transpose-of-matrix.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine().trim());
+        while(t-->0)
+        {
+            int n=Integer.parseInt(in.readLine().trim());
+            int a[][]=new int[n][n];
+            for(int i=0;i<n;i++){
+                String s[]=in.readLine().trim().split(" ");
+                for(int j=0;j<n;j++){
+                    a[i][j]=Integer.parseInt(s[j]);
+                }
+            }
+            Solution ob=new Solution();
+            ob.transpose(n,a);
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    out.print(a[i][j]+" ");
+                }out.println();
+            }
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    public void transpose(int n, int a[][])
+    {
+      int left =0;
+      while(left<n-1){
+          for(int i=left+1; i<n; i++){
+           //Swap row greater than current column with corresponding column 
+           
+           int temp = a[i][left];
+           a[i][left] = a[left][i];
+           a[left][i] = temp;
+           
+          }
+          left++;
+      }
+    }
+}
