6b72e3aedabd35defdb67140d7602b570f32813a
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-09-09 03:03:02 +0530

    Added solution - LeetHub

--- Kth largest element in BST - GFG/kth-largest-element-in-bst.java
+++ Kth largest element in BST - GFG/kth-largest-element-in-bst.java
@@ -114,18 +114,7 @@ class Node
     }
 }
 */
-// class Node
-// {
-//     int data;
-//     Node left;
-//     Node right;
-//     Node(int data)
-//     {
-//         this.data = data;
-//         left=null;
-//         right=null;
-//     }
-// }
+
 
 class Solution
 {
