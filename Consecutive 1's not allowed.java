cbbb9cda797f5ca63e93b0fe8dc1cb6093c408c9
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-12-13 00:30:06 +0530

    Create Consecutive 1's not allowed.java

---
+++ Consecutive 1's not allowed.java
@@ -0,0 +1,16 @@
+class Solution {
+    long countStrings(int n) {
+        int MOD = 1000000007;
+        long zero = 1;
+        long one = 1;
+        
+        for(int i=2;i<=n;i++) {
+            long nZero = (zero + one) % MOD;
+            long nOne = zero;
+            
+            zero = nZero;
+            one = nOne; 
+        }
+        return (int)(zero + one) %MOD;
+    }
+}
