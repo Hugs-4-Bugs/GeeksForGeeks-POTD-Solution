3019e7c97440456d7c853ba1c3ba7dbfe410c06e
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-02-12 00:16:36 +0530

    Create Recursive sequence.java

---
+++ Recursive sequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    static long sequence(int n) {
+        long MOD = 1000000007;
+        long x = 1, ans = 0;
+
+        // Loop through each number in the sequence
+        for (int i = 1; i <= n; ++i) {
+            long temp = 1;
+
+            // Calculate the product of x in each iteration 
+            for (int j = 1; j <= i; ++j) {
+                temp = (temp * x) % MOD;
+                ++x;
+            }
+
+            // Add the product to the sum
+            ans = (ans + temp) % MOD;
+        }
+
+        // Return the sum of the sequence
+        return ans;
+    }
+}
