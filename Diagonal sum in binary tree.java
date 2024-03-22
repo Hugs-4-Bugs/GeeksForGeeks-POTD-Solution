ee54d7df847dfab0d2bfdd3d83f74b00f1ab85fa
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-03-22 15:24:05 +0530

    Create Diagonal sum in binary tree.java

---
+++ Diagonal sum in binary tree.java
@@ -0,0 +1,25 @@
+class Tree {
+    public static ArrayList <Integer> diagonalSum(Node root) 
+    {
+        // code here.
+        ArrayList<Integer>ans= new ArrayList<>();
+        if(root==null) return null;
+        Queue<Node> q= new LinkedList<>();
+        q.offer(root);
+        while(q.size()!=0){
+            int sz=q.size();
+            int sum=0;
+            while(sz-->0){
+                Node rn= q.poll();
+                while(rn!=null){sum+=rn.data;
+                if(rn.left!=null)q.offer(rn.left);
+                rn=rn.right;
+                    
+                }
+            }
+            ans.add(sum);
+            
+        }
+        return ans;
+    }
+}
