8b72bf018619c48d2d1ad8aea065d564d12fe663
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-05-09 21:43:27 +0530

    Create Divisor Game.java

---
+++ Divisor Game.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public static boolean divisorGame(int n) {
+        // code here
+        int count = 0;
+        for(int i=1; i<n; i++){
+            if(n%i == 0){
+                count++;
+            }
+            n = n-i;
+            i=0;
+        }
+        if(count%2 == 0){
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
