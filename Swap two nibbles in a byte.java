0f7e32d489976b69ec457f069fde4be5aa18318b
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-05-31 21:46:15 +0530

    Create Swap two nibbles in a byte.java

---
+++ Swap two nibbles in a byte.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+class Solution {
+    static int swapNibbles(int n) {
+        // code here
+         int first=n%16;  //FIRST NIBBLE
+        int second=n/16; //SECOND NIBBLE
+        return first*16+second;  //COMBINING TWO NIBBLES
+    }
+}
