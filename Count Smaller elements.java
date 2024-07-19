4db49b2843e8bf2ddbd5b1812dd735ea808b1715
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-07-19 16:14:22 +0530

    Create Count Smaller elements.java

---
+++ Count Smaller elements.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    int[] constructLowerArray(int[] arr) {
+        // code here
+        int n= arr.length;
+        int[] ans=new int[n];
+        for(int i=0;i<n-1;i++){
+             int count=0;
+            for(int j=i+1;j<n;j++){
+               if(arr[i]>arr[j]){
+                   count++;
+               }
+            }
+            ans[i]=count;
+        }
+        return ans;
+    }
+}
